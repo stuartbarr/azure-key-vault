fd1fd1ebefb89553e4d2c1cfaf5e6a3b764181aa
Stuart Barr <sbarr@yammer-inc.com>
2015-08-21 11:27:16 +0100

    Shouldn't have to require relative

--- lib/azure_key_vault.rb
+++ lib/azure_key_vault.rb
@@ -1,6 +1,6 @@
-require_relative 'azure_key_vault/key_vault'
-require_relative 'azure_key_vault/key_vault_auth'
-require_relative 'azure_key_vault/key_vault_url'
-require_relative 'azure_key_vault/key_vault_api_version'
+require 'azure_key_vault/key_vault'
+require 'azure_key_vault/key_vault_auth'
+require 'azure_key_vault/key_vault_url'
+require 'azure_key_vault/key_vault_api_version'
 
 require 'rest-client'
