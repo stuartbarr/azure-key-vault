c412a30aa5dc618038c68b8d7c486b04dc2ed3c4
Stuart Barr <sbarr@yammer-inc.com>
2015-08-21 14:21:24 +0100

    Bump version to 0.0.3

--- lib/key_vault/version.rb
+++ lib/key_vault/version.rb
@@ -1,5 +1,5 @@
 module KeyVault
   class Version
-    VERSION = '0.0.2'
+    VERSION = '0.0.3'
   end
 end
--- spec/key_vault/version_spec.rb
+++ spec/key_vault/version_spec.rb
@@ -4,6 +4,6 @@ describe KeyVault::Version do
   let(:version) { KeyVault::Version::VERSION }
   
   it 'gets the correct version' do 
-    expect(version).to eq '0.0.2'
+    expect(version).to eq '0.0.3'
   end
 end
